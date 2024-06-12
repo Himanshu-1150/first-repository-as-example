28d0cee9ed86d76f34fa67179c9f6b1353cea31e
himanshu <rajnirajni3307@gmail.com>
2024-06-12 10:53:41 -0700

    Add files via upload

---
+++ text.js
@@ -0,0 +1,14 @@
+/* javaScript is a 
+synchonous
+single threads 
+
+Execution context   default javaScript 
+execute one line of code at a time 
+console log 1       Each option waits for the last ones to console before execution 
+console log 2 
+Call stack memory heap 
+
+blocking code vs non blocking program 
+block the flow of program road file sync
+does not block execution road file async 
+*/
